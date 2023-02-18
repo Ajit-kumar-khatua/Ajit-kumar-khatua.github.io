0484386775672372f1464941b365f4da9d1b0374
unknown <ajitkumarkhatua286@gmail.com>
2023-02-18 09:47:30 +0530

    portfolio updated

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function toggleMobileMenu(menu){
     menu.classList.toggle("open")
 
 }
+
+let resumeBtn1=document.getElementById("resume-button-1")
+
+resumeBtn1.addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link")
+})
+
+let resumeBtn2=document.getElementById("resume-button-2")
+
+resumeBtn2.addEventListener("click",()=>{
+    window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link")
+})
