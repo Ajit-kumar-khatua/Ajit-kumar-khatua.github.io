6a6d28e96255f9a60ff9a04e960938a22bfec91d
unknown <ajitkumarkhatua286@gmail.com>
2023-03-13 21:24:21 +0530

    portfolio updated

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function toggleMobileMenu(menu){
 
 }
 
-let resumeBtn1=document.getElementById("resume-button-1")
+let resumeBtn1=document.getElementById("resume-link-1")
 
 resumeBtn1.addEventListener("click",()=>{
-    window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link")
+    window.location.assign("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link","_blank")
 })
 
-let resumeBtn2=document.getElementById("resume-button-2")
+let resumeBtn2=document.getElementById("resume-link-2")
 
 resumeBtn2.addEventListener("click",()=>{
-    window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link")
+    window.location.assign("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link","_blank")
 })
