ba36b89c9ecb677d55ce20d1d83402ddbb2901fa
unknown <ajitkumarkhatua286@gmail.com>
2023-03-21 12:13:02 +0530

    updated

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ function toggleMobileMenu(menu){
 }
 
 let resumeBtn1=document.getElementById("resume-link-1")
+resumeBtn1.addEventListener("click",resume)
 
-resumeBtn1.addEventListener("click",()=>{
+function resume(){
     window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link","_blank")
-})
-
-let resumeBtn2=document.getElementById("resume-link-2")
-
-resumeBtn2.addEventListener("click",()=>{
-    window.open("https://drive.google.com/file/d/1gnuqP28KUlnifpn3hQP6eJJ1wnwk7q-8/view?usp=share_link","_blank")
-})
+}
